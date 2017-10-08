d92a52a272d4cf57023f8de6e3fdd4a03a8af6b6
Jared <josejared7616@hotmail.com>
2017-10-08 18:36:57 -0500

    Login completo

--- Desktop/ProyectosAndroid/Prueba1/app/src/main/java/latitude_pc/app/prueba1/com/prueba1/UserAreaActivity.java
+++ Desktop/ProyectosAndroid/Prueba1/app/src/main/java/latitude_pc/app/prueba1/com/prueba1/UserAreaActivity.java
@@ -1,18 +1,32 @@
 package latitude_pc.app.prueba1.com.prueba1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class UserAreaActivity extends AppCompatActivity {
 
+
+public class UserAreaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
 
-        final EditText etUserName = (EditText) findViewById(R.id.etUserName);
-        final TextView welcomMessage = (TextView) findViewById(R.id.tvWelcomeMessage);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        String username = intent.getStringExtra("username");
+        int age = intent.getIntExtra("age", -1);
+
+        TextView tvWelcomeMsg = (TextView) findViewById(R.id.tvWelcomeMessage);
+        EditText etUsername = (EditText) findViewById(R.id.etUserName);
+        EditText etAge = (EditText) findViewById(R.id.etAge);
+
+        // Display user details
+        String message = name + " Bienvenido al area de usuario";
+        tvWelcomeMsg.setText(message);
+        etUsername.setText(username);
+        etAge.setText(age + "");
     }
 }
